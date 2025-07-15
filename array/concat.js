8fdd563a79a24e6f8f3fa13ef37eb0312e274c99
Matheus Pessoa <matheus41.dev@gmail.com>
2025-07-15 07:57:43 -0300

    concat e flatmap

---
+++ array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Antônia', 'Valesca']
+const filhos = ['Felipe', 'Smite']
+
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(filhas, filhos, todos)
+
+console.log([].concat([1, 2], [3, 4], 5))
